6905d2f8dd92c63cad2aeb07ba5139c6946b209a
VivianaGuerraCustodio <vivianaguerracustodio@gmail.com>
2021-01-06 18:05:47 -0500

    template e imagenes de la seccion contacto agregado

--- src/views/contact.js
+++ src/views/contact.js
@@ -1,6 +1,19 @@
 export default () => {
     const contacto = `
-    Pronto encontraras un formulario de contacto aqui
+    <section class="container sobre-nosotros">
+    <div class="custom-contact">
+        <div class="col">
+            <p>
+            Agréganos a nuestra red social o escribenos por whatsapp
+            </p>
+        </div>
+        <div class="row">
+            <img class="col" id="icon" src="./assets/icons/facebook.svg" alt="icono de facebook ">
+            <img class="col" id="icon" src="./assets/icons/whatsapp.svg" alt="icono de whatsapp ">
+            <img class="col" id="icon" src="./assets/icons/telefono.svg" alt="icono de telefono ">
+        </div>
+    </div>
+</section>
     `
     const divElement = document.createElement('div')
     divElement.innerHTML = contacto;
